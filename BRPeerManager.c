b12fed26d200e41a5a2b272db0f5e38d9ae80c53
Russell Cattelan <cattelan@digitalelves.com>
2018-02-23 18:38:52 -0600

    seed node running new protocol

--- BRPeerManager.c
+++ BRPeerManager.c
@@ -77,13 +77,7 @@ static const struct { uint32_t height; const char *hash; uint32_t timestamp; uin
 };
 
 static const char *dns_seeds[] = {
-	/*
-    "seed.breadwallet.com.", "seed.bitcoin.sipa.be.", "dnsseed.bluematt.me.", "dnsseed.bitcoin.dashjr.org.",
-    "seed.bitcoinstats.com.", "bitseed.xf2.org.", "seed.bitcoin.jonasschnelli.ch."
-	*/
-    "xct.sinf.io",
-	"localhost",
-	"hackintosh.x.thebarn.com", "c20.x.thebarn.com"
+	"xtc.sinf.io"
 };
 
 #endif
